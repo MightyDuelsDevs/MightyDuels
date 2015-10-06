d1368d0472c9fa08f6fe254f31309c7d1d075752
MartijnVanBuul <martijn.vanbuul@student.fontys.nl>
2015-10-06 09:54:28 +0200

    Merge origin/master
    Conflicts:
    	src/stamboom/domain/Gezin.java
    	src/stamboom/domain/Persoon.java
    	src/stamboom/gui/StamboomFXController.java

--- src/Mighty_Cards/Hero.java
+++ src/Mighty_Cards/Hero.java
@@ -1,151 +1,181 @@
 package Mighty_Cards;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Hero implements ITarget {
 
-	private Match match;
-	private int hitPoints = 50;
-	private boolean finished;
-        private Deck deck;
-        private Player player;
-        private ArrayList<Minion> minions;
-        private ArrayList<Card> cardsInHand;
-        private Card cardPlayed;
+    private Match match;
+    private int hitPoints = 50;
+    private boolean finished;
+    private Deck deck;
+    private Player player;
+    private ArrayList<Minion> minions;
+    private ArrayList<Card> cardsInHand;
+    private Card cardPlayed;
+    private Random randomCard = new Random();
 
-	/**
-	 * 
-	 * @param match
-	 * @param player
-	 * @param deck
-	 */
-	public Hero(Match match, Player player, Deck deck) {
-		// TODO - implement Hero.Hero
-		this.match = match;
-                this.player = player;
-                this.deck = deck;
-	}
+    /**
+     *
+     * @param match
+     * @param player
+     * @param deck
+     */
+    public Hero(Match match, Player player, Deck deck) {
+        // TODO - implement Hero.Hero
+        this.match = match;
+        this.player = player;
+        this.deck = deck;
+    }
 
-        /**
-         * Gets the deck of the player
-         * @return deck of type Deck
-         */
-	public Deck getDeck() {
-		return deck;
-	}
+    /**
+     * Gets the deck of the player
+     *
+     * @return deck of type Deck
+     */
+    public Deck getDeck() {
+        return deck;
+    }
 
-	/**
-	 * Sets the deck of the player
-	 * @param deck
-	 */
-	public void setDeck(Deck deck) {
-		this.deck = deck;
-	}
+    /**
+     * Sets the deck of the player
+     *
+     * @param deck
+     */
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
 
-        /**
-         * returns if the player is finished with his turn
-         * @return if player is finished with boolean
-         */
-	public boolean getFinished() {
-		return this.finished;
-	}
+    /**
+     * returns if the player is finished with his turn
+     *
+     * @return if player is finished with boolean
+     */
+    public boolean getFinished() {
+        return this.finished;
+    }
 
-	/**
-	 * sets if the player is finished
-	 * @param finished
-	 */
-	public void setFinished(boolean finished) {
-		this.finished = finished;
-	}
+    /**
+     * sets if the player is finished
+     *
+     * @param finished
+     */
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
 
-        /**
-         * returns the minions of the player
-         * @return the minions of the Hero in an ArrayList<Minion>
-         */
-	public ArrayList<Minion> getMinions() {
-		return minions;
-	}
+    /**
+     * returns the minions of the player
+     *
+     * @return the minions of the Hero in an ArrayList<Minion>
+     */
+    public ArrayList<Minion> getMinions() {
+        return minions;
+    }
 
-	/**
-	 * sets the minions of the player
-	 * @param minions
-	 */
-	public void setMinions(ArrayList<Minion> minions) {
-		this.minions = minions;
-	}
+    /**
+     * sets the minions of the player
+     *
+     * @param minions
+     */
+    public void setMinions(ArrayList<Minion> minions) {
+        this.minions = minions;
+    }
 
-        /**
-         * returns the cards of the player
-         * @return the cards of the Hero in an ArrayList<Card>
-         */
-	public ArrayList<Card> getInHand() {
-		return cardsInHand;
-	}
+    /**
+     * returns the cards of the player
+     *
+     * @return the cards of the Hero in an ArrayList<Card>
+     */
+    public ArrayList<Card> getInHand() {
+        return cardsInHand;
+    }
 
-	/**
-	 * sets the hand of the player
-	 * @param inHand
-	 */
-	public void setNewHand(ArrayList<Card> inHand) {
-		this.cardsInHand = inHand;
-	}
+    /**
+     * sets the hand of the player
+     *
+     * @param inHand
+     */
+    public void setNewHand(ArrayList<Card> inHand) {
+        this.cardsInHand = inHand;
+    }
 
-        /**
-         * returns the card played by the player
-         * @return the card played by the Hero as Card
-         */
-	public Card getCardPlayed() {
-		return cardPlayed;
-	}
+    /**
+     * returns the card played by the player
+     *
+     * @return the card played by the Hero as Card
+     */
+    public Card getCardPlayed() {
+        return cardPlayed;
+    }
 
-	/**
-	 * sets the card played by the Hero
-	 * @param cardPlayed
-	 */
-	public void setCardPlayed(Card cardPlayed) {
-		this.cardPlayed = cardPlayed;
-	}
+    /**
+     * sets the card played by the Hero
+     *
+     * @param cardPlayed
+     */
+    public void setCardPlayed(Card cardPlayed) {
+        this.cardPlayed = cardPlayed;
+    }
 
-        /**
-	 * grabs cards from the deck and sets the cards in hand.
-         * it uses the setNewHand method
-	 */
-	public void pullCards() {
-		// TODO - implement Hero.pullCards
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * grabs cards from the deck and sets the cards in hand. it uses the
+     * setNewHand method
+     */
+    public void pullCards() {
+        ArrayList<Card> cards = deck.getCards();
+        ArrayList<Card> cardsInHand = new ArrayList<>();
 
-	/**
-	 * tries to play the card the Player selected
-	 * @param card
-         * @return if the method succeeded
-	 */
-	public boolean playCard(Card card) {
-		// TODO - implement Hero.playCard
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * removes the minions from the ArrayList of minions
-	 * @param minion
-	 */
-	public void removeMinion(Minion minion) {
-		minions.remove(minion);
-	}
-        
-        /**
-         * returns the current hitPoints of the Hero
-	 * @return the hitPoints of the Hero
-	 */
-        public int getHitPoints(){
-            return hitPoints;
+        for (int i = 1; i <= 3; i++) {
+            int cardPosition = randomCard.nextInt(29);
+            cardsInHand.add(cards.get(cardPosition));
+            cards.remove(cardPosition);
         }
-        
-        /**
-         * sets the hitPoints of the Hero
-	 * @param hitPoints
-	 */
-        public void SetHitPoints(int hitPoints){
-            this.hitPoints = hitPoints;
+
+        this.setNewHand(cardsInHand);
+    }
+
+    /**
+     * tries to play the card the Player selected
+     *
+     * @param card
+     * @return if the method succeeded
+     */
+    public boolean playCard(Card card) {
+        if (this.minions.size() >= 2) {
+            if (card instanceof MinionCard) {
+                return false;
+            }
         }
+
+        this.setCardPlayed(card);
+        return true;
+    }
+
+    /**
+     * removes the minions from the ArrayList of minions
+     *
+     * @param minion
+     */
+    public void removeMinion(Minion minion) {
+        minions.remove(minion);
+    }
+
+    /**
+     * returns the current hitPoints of the Hero
+     *
+     * @return the hitPoints of the Hero
+     */
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    /**
+     * sets the hitPoints of the Hero
+     *
+     * @param hitPoints
+     */
+    public void SetHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
 }
