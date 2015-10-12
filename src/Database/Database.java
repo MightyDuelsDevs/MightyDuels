c794d9bb3dbadcfc15f9babecff3b817c0a11771
MartijnVanBuul <martijn.vanbuul@student.fontys.nl>
2015-10-12 11:58:36 +0200

    Tried to make database class

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -5,10 +5,40 @@
  */
 package Database;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  *
  * @author Martijn
  */
 public class Database {
     
+    public static void main(String[] argv) {
+
+        System.out.println("-------- Oracle JDBC Connection Testing ------");
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:oracle:simplefan:@192.168.2.14:1521:orcl", "MightyDuels", "MSPW");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
+    }
 }
