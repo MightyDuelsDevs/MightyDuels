a179caa3cee5efa09077c519e36970d8961c365b
Rick Rongen <r.rongen@student.fontys.nl>
2015-10-14 17:58:09 +0200

    implemented match.processTurn(), added comment in Minion.attack() missing rule I23

--- src/Mighty_Cards/Domain/Match.java
+++ src/Mighty_Cards/Domain/Match.java
@@ -1,5 +1,7 @@
 package Mighty_Cards.Domain;
 
+import java.util.ArrayList;
+
 public class Match {
 
 	private int turns;
@@ -28,8 +30,65 @@ public class Match {
 	}
 
 	private void processTurn() {
-            // TODO - implement Match.processTurn
-            throw new UnsupportedOperationException();
+            Card p1 = hero1.getCardPlayed();
+            Card p2 = hero2.getCardPlayed();
+            
+            //check minion card
+            //create new minions
+            if(p1 instanceof MinionCard){
+                Minion m = new Minion((MinionCard)p1);
+                //hero1.addMinion(m);
+                //todo set target somhow?
+            }
+            if(p2 instanceof MinionCard){
+                Minion m = new Minion((MinionCard)p2);
+                //hero2.addMinion(m);
+                //todo set target somhow?
+            }
+            
+            //get all active minions
+            ArrayList<Minion> p1min = hero1.getMinions();
+            ArrayList<Minion> p2min = hero2.getMinions();
+            
+            //all not player attacks            
+            p1min.stream().filter((m)->m.getITarget()!=hero2).forEach((m)->m.Attack());//attack all not players
+            p2min.stream().filter((m)->m.getITarget()!=hero1).forEach((m)->m.Attack());//attack all not players
+            
+            //remove dead minions
+            p1min.removeIf((m)->m.getHitPoints()<=0);
+            p2min.removeIf((m)->m.getHitPoints()<=0);
+            
+            //all minion to player attacks
+            p1min.stream().filter((m)->m.getITarget()==hero2).forEach((m)->m.Attack());//attack hero2
+            p2min.stream().filter((m)->m.getITarget()==hero1).forEach((m)->m.Attack());//attack hero1
+            
+            //attack using cards
+            //attack player card
+            if(p1 instanceof HeroCard){
+                HeroCard p1h = (HeroCard)p1;
+                int hp = hero2.getHitPoints();
+                int pSchield = 0, mSchield=0;
+                if(p2 instanceof HeroCard){
+                    HeroCard p2h = (HeroCard)p2;
+                    pSchield = p2h.getPhysicalBlock();
+                    mSchield = p2h.getMagicalBlock();
+                }
+                hero2.SetHitPoints(hp + (pSchield - p1h.getPhysicalBlock()) + (mSchield - p1h.getMagicalDamage()));
+            }
+            if(p2 instanceof HeroCard){
+                HeroCard p2h = (HeroCard)p2;
+                int hp = hero1.getHitPoints();
+                int pSchield = 0, mSchield=0;
+                if(p1 instanceof HeroCard){
+                    HeroCard p1h = (HeroCard)p1;
+                    pSchield = p1h.getPhysicalBlock();
+                    mSchield = p1h.getMagicalBlock();
+                }
+                hero2.SetHitPoints(hp + (pSchield - p2h.getPhysicalBlock()) + (mSchield - p2h.getMagicalDamage()));
+            }
+            
+            //todo here or somwere else?
+            determineGameState();
 	}
 
 	/**
