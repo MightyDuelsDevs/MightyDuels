0af9c8ed1275d79f4ce2f3f7cfd7d850d19cdcef
Boon,Matthijs M. van der <m.vanderboon@student.nl>
2015-11-02 09:24:22 +0100

    Duel toevoegen (zonder navigatie)

---
+++ src/GUI/GUIMatchController.java
@@ -0,0 +1,156 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import Controller.CardDeckController;
+import Mighty_Cards.Domain.Card;
+import Mighty_Cards.Domain.HeroCard;
+import Mighty_Cards.Domain.Match;
+import Mighty_Cards.Domain.Player;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Matthijs
+ */
+public class GUIMatchController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML GridPane gridPlayedCards;
+    @FXML GridPane gridChooseCard;
+    @FXML GridPane gridYourSide;
+    @FXML GridPane gridOpponentSide;
+    
+    private ArrayList<HeroCard> cards;
+    private ArrayList<HeroCardControl> heroCardControls;
+    private boolean yourCardPlayed = false;
+    private HeroControl yourHero;
+    private HeroControl opponentsHero;
+    
+    private Player player_1;
+    private Player player_2;
+    
+    private Match match;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        heroCardControls = new ArrayList<>();
+        cards = new ArrayList<>();
+        InitialiseHeroes();
+        match = new Match(player_1);
+        match.addPlayer2(player_2);
+        
+        for(Card card : CardDeckController.allCards){
+            if(card instanceof HeroCard){
+                cards.add((HeroCard)card);
+            }
+        }
+        
+        drawCards();
+    }    
+    
+    public void InitialiseHeroes(){
+        player_1 = new Player(1, "Stan", 1, 1, 0, 0, 0);
+        player_2 = new Player(2, "Ramòn", 2, 2, 0, 0, 0);
+        
+        Random random = new Random();
+        
+        yourHero = new HeroControl(50,player_1);
+        gridYourSide.add(yourHero.getHeroControl(), 0, 0);
+        
+        opponentsHero = new HeroControl(50,player_2);
+        gridOpponentSide.add(opponentsHero.getHeroControl(), 4, 0);
+        
+        ImageView imageViewYourDeck = new ImageView("/Images/card stack.png");
+        ImageView imageViewOpponentsDeck = new ImageView("/Images/card stack.png");
+        
+        gridYourSide.add(imageViewYourDeck, 4, 0);
+        gridOpponentSide.add(imageViewOpponentsDeck, 0, 0);
+    }
+    
+    private void drawCards(){
+        Random random = new Random();
+        heroCardControls.clear();
+        
+        for(int i = 0; i <3;i++){
+            int count = i;
+                        
+            
+            boolean added = false;
+            
+            while(!added){
+                HeroCardControl control = new HeroCardControl(cards.get(random.nextInt(cards.size())), 180); 
+                if(!heroCardControls.contains(control)){
+                    heroCardControls.add(control);
+                    added = true;
+                }
+            }
+
+            EventHandler handler = new EventHandler() {
+                StackPane pane = heroCardControls.get(count).HeroCardControlPane();
+
+                @Override
+                    public void handle(Event event) {
+                        if(!yourCardPlayed){
+                            yourCardPlayed = true;
+
+                            gridYourSide.add(pane, 1, 0);
+                            gridChooseCard.getChildren().clear();
+                            match.getHero1().setCardPlayed((Card)heroCardControls.get(count).getHeroCard());
+                            match.getHero1().setFinished(true);
+                            drawCards();
+                        }
+                        else if(yourCardPlayed){
+                            
+                            yourCardPlayed = false;
+
+                            gridOpponentSide.add(pane, 1, 0);
+                            boolean proceed = false;
+                            while(!proceed){
+                                if(gridOpponentSide.getChildren().contains(pane)){
+                                    proceed = true;
+                                }
+                            }
+                            gridChooseCard.getChildren().clear();
+
+                            match.getHero2().setCardPlayed((Card)heroCardControls.get(count).getHeroCard());
+                            match.getHero2().setFinished(true);
+                            
+                            match.StartTurn();
+
+                            
+                            gridYourSide.getChildren().remove(2);
+                            gridOpponentSide.getChildren().remove(2);
+                            
+                            yourHero.setHealth(match.getHero1().getHitPoints());
+                            opponentsHero.setHealth(match.getHero1().getHitPoints());
+                            drawCards();
+                        }
+                    }
+                };
+            heroCardControls.get(count).setEventHandler(handler);
+            gridChooseCard.add(heroCardControls.get(i).HeroCardControlPane(), i, 0);
+        }
+    }
+}
--- src/Mighty_Cards/Domain/Match.java
+++ src/Mighty_Cards/Domain/Match.java
@@ -3,6 +3,7 @@ package Mighty_Cards.Domain;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -46,81 +47,121 @@ public class Match {
          * Processes one turn. (card's played, minion attacks and gamestate update)
          */
 	private void processTurn() {
-            log.info(String.format("Processing turn %d of player '%s' and '%s'.",turns,player1.getUsername(),player2.getUsername()));
+            //log.info(String.format("Processing turn %d of player '%s' and '%s'.",turns,player1.getUsername(),player2.getUsername()));
             Card p1 = hero1.getCardPlayed();
             Card p2 = hero2.getCardPlayed();
             
             //check minion card
             //create new minions
             if(p1 instanceof MinionCard){
-                log.info(String.format("Adding minion %s to %s",p1.getName(),player1.getUsername()));
+                //log.info(String.format("Adding minion %s to %s",p1.getName(),player1.getUsername()));
                 Minion m = new Minion((MinionCard)p1);
                 hero1.getMinions().add(m);
                 //hero1.addMinion(m);
                 //todo set target somhow?
             }
             if(p2 instanceof MinionCard){
-                log.info(String.format("Adding minion %s to %s",p2.getName(),player2.getUsername()));
+                //log.info(String.format("Adding minion %s to %s",p2.getName(),player2.getUsername()));
                 Minion m = new Minion((MinionCard)p2);
                 hero2.getMinions().add(m);
                 //hero2.addMinion(m);
                 //todo set target somhow?
             }
             
-            log.info("Processing minion attacks. Obtaining minions");
+            //log.info("Processing minion attacks. Obtaining minions");
             //get all active minions
             ArrayList<Minion> p1min = hero1.getMinions();
             ArrayList<Minion> p2min = hero2.getMinions();
             
-            log.info("Filter non hero attack, attack minions");
+            //log.info("Filter non hero attack, attack minions");
             //all not player attacks            
             p1min.stream().filter((m)->m.getITarget()!=hero2).forEach((m)->m.Attack());//attack all not players
             p2min.stream().filter((m)->m.getITarget()!=hero1).forEach((m)->m.Attack());//attack all not players
             
-            log.info("Remove dead minions");
+            //log.info("Remove dead minions");
             //remove dead minions
             p1min.removeIf((m)->m.getHitPoints()<=0);
             p2min.removeIf((m)->m.getHitPoints()<=0);
             
-            log.info("Filter hero attacks, attack hero");
+            //log.info("Filter hero attacks, attack hero");
             //all minion to player attacks
             p1min.stream().filter((m)->m.getITarget()==hero2).forEach((m)->m.Attack());//attack hero2
             p2min.stream().filter((m)->m.getITarget()==hero1).forEach((m)->m.Attack());//attack hero1
             
-            log.info("Process HeroCard attacks");
+            //log.info("Process HeroCard attacks");
             //attack using cards
             //attack player card
             if(p1 instanceof HeroCard){
-                log.info(String.format("%s attacks %s with %s",player1.getUsername(),player2.getUsername(),p1.getName()));
+                //log.info(String.format("%s attacks %s with %s",player1.getUsername(),player2.getUsername(),p1.getName()));
                 HeroCard p1h = (HeroCard)p1;
+                
                 int hp = hero2.getHitPoints();
-                int pSchield = 0, mSchield=0;
+                //Matthijs
+                int pSchield = 0, mSchield=0, healing = 0, physicalDamageDone = 0, magicalDamageDone = 0, totalValue = 0;
                 if(p2 instanceof HeroCard){
-                    log.info(String.format("%s blocks %s with %s",player2.getUsername(),player1.getUsername(),p2.getName()));
+                    //log.info(String.format("%s blocks %s with %s",player2.getUsername(),player1.getUsername(),p2.getName()));
                     HeroCard p2h = (HeroCard)p2;
                     pSchield = p2h.getPhysicalBlock();
                     mSchield = p2h.getMagicalBlock();
+                    healing = p2h.getHealValue();
                 }
-                log.info(String.format("hero2hp: %d, hero2PysicalSchield: %d, hero1PhysicalDamage: %d, hero2MagicalSchield: %d, hero1MagicalDamage: %d",hp,pSchield,p1h.getPhysicalDamage(),mSchield,p1h.getMagicalDamage()));
-                hero2.SetHitPoints(hp + (pSchield - p1h.getPhysicalDamage()) + (mSchield - p1h.getMagicalDamage()));
+                physicalDamageDone = p1h.getPhysicalDamage() - pSchield;
+                if(physicalDamageDone < 0){
+                    physicalDamageDone = 0;
+                }
+                magicalDamageDone = p1h.getMagicalDamage() - mSchield;
+                if(magicalDamageDone < 0){
+                    magicalDamageDone = 0;
+                }
+                totalValue = (physicalDamageDone + magicalDamageDone) + healing;
+                if(hp - totalValue > 50){
+                    hero2.SetHitPoints(50);
+                }
+                else{
+                    hero2.SetHitPoints(hp - totalValue);
+                }
+                
+                System.out.println("hero 2 heeft "+ hero2.getHitPoints() + " hp over");
+                //Matthijs
             }
             if(p2 instanceof HeroCard){
-                log.info(String.format("%s attacks %s with %s",player2.getUsername(),player1.getUsername(),p2.getName()));
+                //log.info(String.format("%s attacks %s with %s",player2.getUsername(),player1.getUsername(),p2.getName()));
                 HeroCard p2h = (HeroCard)p2;
                 int hp = hero1.getHitPoints();
-                int pSchield = 0, mSchield=0;
+                //Matthijs
+                int pSchield = 0, mSchield=0, healing = 0, physicalDamageDone = 0, magicalDamageDone = 0, totalValue = 0;
                 if(p1 instanceof HeroCard){
-                    log.info(String.format("%s blocks %s with %s",player1.getUsername(),player2.getUsername(),p1.getName()));
+                    //log.info(String.format("%s blocks %s with %s",player1.getUsername(),player2.getUsername(),p1.getName()));
                     HeroCard p1h = (HeroCard)p1;
                     pSchield = p1h.getPhysicalBlock();
                     mSchield = p1h.getMagicalBlock();
+                    healing = p1h.getHealValue();
                 }
-                log.info(String.format("hero2hp: %d, hero2PysicalSchield: %d, hero1PhysicalDamage: %d, hero2MagicalSchield: %d, hero1MagicalDamage: %d",hp,pSchield,p2h.getPhysicalDamage(),mSchield,p2h.getMagicalDamage()));
-                hero2.SetHitPoints(hp + (pSchield - p2h.getPhysicalBlock()) + (mSchield - p2h.getMagicalDamage()));
+                physicalDamageDone = p2h.getPhysicalDamage() - pSchield;
+                if(physicalDamageDone < 0){
+                    physicalDamageDone = 0;
+                }
+                magicalDamageDone = p2h.getMagicalDamage() - mSchield;
+                if(magicalDamageDone < 0){
+                    magicalDamageDone = 0;
+                }
+                totalValue = (physicalDamageDone + magicalDamageDone) + healing;
+                if(hp - totalValue > 50){
+                    hero1.SetHitPoints(50);
+                }
+                else{
+                    hero1.SetHitPoints(hp - totalValue);
+                }
+                System.out.println("hero 1 heeft "+ hero1.getHitPoints() + " hp over");
+                //Matthijs
             }
             log.info("Turn finished");
             //todo here or somwere else?
             determineGameState();
+            //Matthijs
+            hero1.setFinished(false);
+            hero2.setFinished(false);
+            //Matthijs
             turns++;
 	}
 
@@ -133,21 +174,23 @@ public class Match {
             hero1 = new Hero(this,player1,new Deck());//todo deck
             determineGameState();
             //gameState = GameState.Waiting;
-            timer = new Timer();
-            timer.schedule(new TimerTask(){
-                @Override
-                public void run() {
-                    if(gameState != GameState.Active){
-                        if(gameState == GameState.Defined || gameState == GameState.Tie){
-                            timer.cancel();//quit if the game has finished
-                            return;
-                        }
-                    }
-                    if(hero1.getFinished() && hero2.getFinished()){
-                        processTurn();
-                    }
-                }
-            }, 0, 100);
+            //Matthijs
+//            timer = new Timer();
+//            timer.schedule(new TimerTask(){
+//                @Override
+//                public void run() {
+//                    if(gameState != GameState.Active){
+//                        if(gameState == GameState.Defined || gameState == GameState.Tie){
+//                            timer.cancel();//quit if the game has finished
+//                            return;
+//                        }
+//                    }
+//                    if(hero1.getFinished() && hero2.getFinished()){
+//                        processTurn();
+//                    }
+//                }
+//            }, 0, 100);
+            //Matthijs
 	}
 
 	/**
@@ -202,5 +245,16 @@ public class Match {
             hero.SetHitPoints(0);
             determineGameState();
 	}
-
+        
+        public void StartTurn(){
+            determineGameState();
+            if(gameState != GameState.Active){
+                if(gameState == GameState.Defined || gameState == GameState.Tie){
+                    System.exit(0);
+                }
+            }
+            if(hero1.getFinished() && hero2.getFinished()){
+                processTurn();
+            }
+        }
 }
