95e5c1ca89adce4d80fb960f0c99b59cbfdd8ddb
0Rick0 <>
2015-11-02 12:33:48 +0100

    fixed most findbugs in ITarget

--- src/Mighty_Cards/Domain/ITarget.java
+++ src/Mighty_Cards/Domain/ITarget.java
@@ -1,13 +1,20 @@
 package Mighty_Cards.Domain;
 
+/**
+ * An interface specifieing someone that can take damage
+ */
 public interface ITarget {
 
 	/**
-	 * 
-	 * @param hitPoints
+	 * Set the hitpoints of the target
+	 * @param hitPoints the new amount of hitpoints
 	 */
 	void setHitPoints(int hitPoints);
 
+        /**
+         * Get the amount of hitpoints the target has
+         * @return the amount of hitpoints
+         */
 	int getHitPoints();
 
 }
